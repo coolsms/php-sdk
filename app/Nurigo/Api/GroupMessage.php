b43d210055bad6d78323721ed4ebe2aa107f8655
BJRambo <qw5414@naver.com>
2017-08-02 13:46:53 +0900

    그룹 메세지 및 일부 동작에 json을 미리 인코딩해서 필요한 정보만 내보내도록 개선

--- app/Nurigo/Api/GroupMessage.php
+++ app/Nurigo/Api/GroupMessage.php
@@ -36,7 +36,7 @@ class GroupMessage extends Coolsms
     /**
      * @brief get group list ( HTTP Method GET )
      * @param None
-     * @return array['groupid', 'groupid'...]
+     * @return object(groupList)
      */
     public function getGroupList()
     {
@@ -84,21 +84,13 @@ class GroupMessage extends Coolsms
         $options->group_id = $group_id;
         return $this->request(sprintf('group/%s/getGroupInfo', $group_id), $options);
     }
-    
+
+
     /**
      * @brief add messages to group ( HTTP Method POST )
-     * @param object $options {
-     *   @param string  group_id [required]
-     *   @param string  to       [required]
-     *   @param string  from     [required]
-     *   @param string  text     [required]
-     *   @param string  image_id [optional]
-     *   @param string  refname  [optional]
-     *   @param string  country  [optional]
-     *   @param string  datetime [optional]
-     *   @param string  subject  [optional]
-     *   @param integer delay    [optional] }
-     * @return object(success_count, error_count, error_list['index':'code', 'index', 'code'])
+     * @param $options
+     * @return mixed
+     * @throws CoolsmsSDKException
      */
     public function addMessages($options) 
     {
@@ -142,35 +134,6 @@ class GroupMessage extends Coolsms
     }
 
     /**
-     * @brief add json type messages to group ( HTTP Method POST )
-     * @param object $options {
-     *   @param string  group_id [required]
-     *   @param string  messages [required] [{
-     *     @param string  to       [required]
-     *     @param string  from     [required]
-     *     @param string  text     [required]
-     *     @param string  image_id [optional]
-     *     @param string  refname  [optional]
-     *     @param string  country  [optional]
-     *     @param string  datetime [optional]
-     *     @param string  subject  [optional]
-     *     @param integer delay    [optional] }] }
-     * @return array[object(success_count, error_count, error_list['index':'code', 'index', 'code']), ...]
-     */
-    public function addMessagesJSON($options) 
-    {
-        if (!isset($options->group_id) || !isset($options->messages)) throw new CoolsmsSDKException('group_id and messages is required', 202);
-        foreach ($options->messages as $val) {
-            if (!isset($val->to) || !isset($val->text) || !isset($val->from)) {
-                throw new CoolsmsSDKException('to, text, from is required', 202);
-            }
-        }
-
-        $options->messages = json_encode($options->messages);
-        return $this->request(sprintf('groups/%s/add_messages.json', $options->group_id), $options, true);
-    }
-
-    /**
      * @brief get message list ( HTTP Method GET )
      * @param string  $group_id [required]
      * @param integer $offset   [optional]
@@ -182,10 +145,13 @@ class GroupMessage extends Coolsms
         if (!$group_id) throw new CoolsmsSDKException('group_id is required', 202);
 
         $options = new \stdClass();
-        $options->group_id = $group_id;
         $options->offset = $offset;
         $options->limit = $limit;
-        return $this->request(sprintf('groups/%s/message_list', $options->group_id), $options);
+
+        $encoding_json_data = json_encode($options);
+        $obj = new \stdClass();
+        $obj->encoding_json_data = $encoding_json_data;
+        return $this->request(sprintf('group/%s/getMessageList', $group_id), $obj, true);
     }
 
     /**
@@ -199,9 +165,14 @@ class GroupMessage extends Coolsms
         if (!$group_id || !$message_ids) throw new CoolsmsSDKException('group_id and message_ids are required', 202);
 
         $options = new \stdClass();
-        $options->group_id = $group_id;
-        $options->message_ids = $message_ids;
-        return $this->request(sprintf('groups/%s/delete_messages', $options->group_id), $options, true);
+        $options->messages = array();
+        $options->messages[0] = new \stdClass();
+        $options->messages[0]->messageId = $message_ids;
+
+        $encoding_json_data = json_encode($options);
+        $obj = new \stdClass();
+        $obj->encoding_json_data = $encoding_json_data;
+        return $this->request(sprintf('groups/%s/deleteMessages', $group_id), $obj, true);
     }
 
     /**
@@ -213,8 +184,8 @@ class GroupMessage extends Coolsms
     {
         if (!$group_id) throw new CoolsmsSDKException('group_id is required', 202);
 
-        $options = new \stdClass();
-        $options->group_id = $group_id;
-        return $this->request(sprintf('group/%s/sendMessages', $group_id), $options, true);
+        $obj = new \stdClass();
+        $obj->encoding_json_data = null;
+        return $this->request(sprintf('group/%s/sendMessages', $group_id), $obj, true);
     }
 }
